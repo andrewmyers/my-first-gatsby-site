1e44459778db2d07b0f69137057636b327cc526d
Andrew Myers <andrewshannonmyers@gmail.com>
2021-11-04 21:08:19 -0500

    Add material-ui styles using the gatsby plugin

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,34 +1,51 @@
-import { Link } from "gatsby";
 import {
-  container,
-  heading,
-  navLinks,
-  navLinkItem,
-  navLinkText,
-} from "./layout.module.css";
+  Typography,
+  AppBar,
+  Toolbar,
+  Link,
+  CssBaseline,
+  Box,
+  Paper,
+} from "@mui/material";
+import { Link as GatsbyLink } from "gatsby";
+import { container, heading, navLinkText } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
   return (
-    <div className={container}>
+    <div>
       <title>{pageTitle}</title>
-      <nav>
-        <ul className={navLinks}>
-          <li className={navLinkItem}>
-            <Link to="/" className={navLinkText}>
-              Home
-            </Link>
-          </li>
-          <li className={navLinkItem}>
-            <Link to="/about" className={navLinkText}>
-              About
-            </Link>
-          </li>
-        </ul>
-      </nav>
-      <main>
-        <h1 className={heading}>{pageTitle}</h1>
+      <CssBaseline />
+      <Box component="main" padding="1rem" maxWidth={500} margin="0 auto">
+        <Box component="nav" marginBottom="1rem">
+          <Link
+            component={GatsbyLink}
+            to="/"
+            className={navLinkText}
+            color="inherit"
+            underline="none"
+          >
+            Home
+          </Link>
+          <Link
+            component={GatsbyLink}
+            to="/about"
+            className={navLinkText}
+            color="inherit"
+            underline="none"
+          >
+            About
+          </Link>
+        </Box>
+        <Typography
+          variant="h3"
+          component="h1"
+          gutterBottom
+          className={heading}
+        >
+          {pageTitle}
+        </Typography>
         {children}
-      </main>
+      </Box>
     </div>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
+import { Typography } from "@mui/material";
 import { StaticImage } from "gatsby-plugin-image";
 import Layout from "../components/Layout";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home">
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I&apos;m making this by following the Gatsby Tutorial.</p>
+      <Typography variant="h4" component="p" gutterBottom>Welcome to my Gatsby site!</Typography>
+      <Typography variant="body1" gutterBottom>I&apos;m making this by following the Gatsby Tutorial.</Typography>
       <StaticImage
         src="https://www.gatsbyjs.com/static/00cce85e5293015b32d90b17e18afdaf/416ee/index-page-with-static-image-from-url.png"
         alt="Image of an image showing on the site i'm making"
